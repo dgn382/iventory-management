f483dfa1ec01558c306fe0d27d02648dc1b5e57b
Nguyen Dung <dungne558@gmail.com>
2026-06-18 15:27:40 +0700

    Initial project setup

---
+++ backend/src/routes/product.routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const productController = require('../controllers/product.controller');
+const { verifyToken } = require('../middlewares/auth.middleware');
+
+// Các đường dẫn API cho sản phẩm
+router.get('/', verifyToken, productController.getAllProducts);
+router.get('/barcode/:barcode', verifyToken, productController.getProductByBarcode);
+router.post('/', verifyToken, productController.createProduct);
+
+router.put('/:id', verifyToken, productController.updateProduct);
+router.delete('/:id', verifyToken, productController.deleteProduct);
+module.exports = router;
